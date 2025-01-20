7b1badd0378f0562ba4d286391613df1f636f40f
Philipp Ullmann <philipp.ullmann@outlook.at>
2025-01-20 16:11:15 +0100

    Refactored boostrap.php file

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-// bootstrap.php
+
 use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
@@ -7,18 +7,11 @@ use JsonMapper\JsonMapperFactory;
 
 require_once "vendor/autoload.php";
 
-// Create a simple "default" Doctrine ORM configuration for Attributes
 $config = ORMSetup::createAttributeMetadataConfiguration(
     paths: [__DIR__ . '/src'],
     isDevMode: true,
 );
-// or if you prefer XML
-// $config = ORMSetup::createXMLMetadataConfiguration(
-//    paths: [__DIR__ . '/config/xml'],
-//    isDevMode: true,
-//);
 
-// configuring the database connection
 $connection = DriverManager::getConnection([
     'driver' => 'pdo_mysql',
     'dbname' => 'development',
@@ -27,9 +20,7 @@ $connection = DriverManager::getConnection([
     'host' => 'localhost',
 ], $config);
 
-// obtaining the entity manager
 $entityManager = new EntityManager($connection, $config);
-
 $jsonMapper = (new JsonMapperFactory())->bestFit();
 $apiClient = new ApiClient($jsonMapper);
 
